b4c90d69b0d1ea5be98e0381ea0d8e9aa3868013
Victoria Meng <nights1ststar@protonmail.com>
2018-03-03 01:25:50 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,10 +26,6 @@ end
 def printer(names)
   badges = batch_badge_creator(names)
   rooms = assign_rooms(names)
-  binding.pry
   badges.each { |badge| puts badge }
   rooms.each { |room| puts room }
 end
-
-names = %w(Alice Bob)
-printer(names)
