8796b950f277cc85f331ff2407fc455d6518a03d
Victoria Meng <nights1ststar@protonmail.com>
2018-03-03 00:42:40 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,8 +7,8 @@ end
 
 def batch_badge_creator(names)
   badges = []
-  names.each do |name| 
-    badges << badge_maker(name) 
+  names.each do |name|
+    badges << badge_maker(name)
   end
   badges
 end
